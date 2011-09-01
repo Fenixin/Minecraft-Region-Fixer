a3271bc185f9dc227e7e4d08d42675b70951a7dd
Alejandro Aguilera <fenixin@lavabit.com>
2011-09-01 13:18:31 +0200

    Created world.py and scan.py, first working version with this configuration.
    Lot of stuff to be done yet!

--- region-fixer.py
+++ region-fixer.py
@@ -29,16 +29,9 @@ import nbt.region as region
 import zlib
 import gzip
 import nbt.nbt as nbt
-import progressbar
-import multiprocessing
-import time
 
-class FractionWidget(progressbar.ProgressBarWidget):
-    def __init__(self, sep=' / '):
-        self.sep = sep
-        
-    def update(self, pbar):
-        return '%2d%s%2d' % (pbar.currval, self.sep, pbar.maxval)
+import world
+from scan import scan_all_players, scan_level, scan_level, scan_all_mcr_files
 
 def parse_backup_list(world_backup_dirs):
     """ Generates a list with the input of backup dirs, also check if
@@ -53,8 +46,6 @@ def parse_backup_list(world_backup_dirs):
             print "[ATTENTION] The directory \"{0}\" is not a minecraft world directory".format(dir)
     return region_backup_dirs
 
-
-
 def parse_chunk_list(chunk_list, region_dir):
     """ Fenerate a list of chunk as done by check_region_file
         but using a list of global chunk coords tuples as input"""
@@ -65,117 +56,6 @@ def parse_chunk_list(chunk_list, region_dir):
         parsed_list.append((fullpath, chunk[0], chunk[1]))
 
     return parsed_list
-		
-
-def check_region_file(region_file, delete_entities = False, entity_limit = 500):
-    """ Takes a RegionFile obj and returns a list of corrupted 
-    chunks where each element represents a corrupted chunk and
-    contains a tuple:
-     (region file, coord x, coord y) 
-     and returns also the total number of chunks (including corrupted ones)
-     """
-
-    total_chunks = 0
-    bad_chunks = []
-    wrong_located_chunks = []
-    try:
-        for x in range(32):
-            for z in range(32):
-                #~ print "In chunk ({0},{1})".format(x,z)
-                chunk = check_chunk(region_file, x, z)
-                if isinstance(chunk, nbt.TAG_Compound):
-                    total_chunks += 1
-                    total_entities = len(chunk['Level']['Entities'].tags)
-                    # deleting entities is here because to parse a chunk with thousands of wrong entities
-                    # takes a long time, and once detected is better to fix it at once.
-                    if delete_entities == True and total_entities > entity_limit:
-                        
-                        #~ print len(chunk['Level']['Entities'].tags)
-                        empty_tag_list = nbt.TAG_List(nbt.TAG_Byte,'','Entities')
-
-                        chunk['Level']['Entities'] = empty_tag_list
-                        #~ print len(chunk['Level']['Entities'].tags)
-                        print "Deleted {0} entities in chunk ({1},{2}).".format(total_entities, x, z)
-                        region_file.write_chunk(x, z, chunk)
-                    
-                    elif total_entities > entity_limit:
-                        print "[WARNING!]: The chunk ({0},{1}) in region file {2} has {3} entities, and this may be too much. This may be a problem!".format(x,z,region_file,total_entities)
-                        
-                elif chunk == -1:
-                    total_chunks += 1
-                    bad_chunks.append((region_file.filename,x,z))
-                elif chunk == -2:
-                    total_chunks += 1
-                    wrong_located_chunks.append((region_file.filename,x,z))
-                # if None do nothing
-                del chunk # unload chunk from memory
-                
-        filename = split(region_file.filename)[1]
-
-        del region_file
-        
-    except KeyboardInterrupt:
-        print "\nInterrupted by user\n"
-        sys.exit(1)
-        
-    return filename,bad_chunks, wrong_located_chunks, total_chunks
-
-def _mp_check_region_file(region_file):
-    counter.value += 1
-    if getsize(region_file) is not 0:
-        r = check_region_file(region.RegionFile(region_file),delete_entities,entity_limit)
-        _mp_check_region_file.q.put(r)
-        return r
-    else:
-        return None
-
-def check_chunk(region_file, x, z):
-    """ Returns the chunk if exists, -1 if it's corrupted, -2 if it the
-    header coords doesn't match the coords storedn inside the chunk
-     and None if it doesn't exist.
-    """
-    try:
-        chunk = region_file.get_chunk(x,z)
-        if chunk:
-            data_coords = get_chunk_data_coords(chunk)
-            header_coords = get_global_chunk_coords(region_file.filename, x, z)
-            if data_coords != header_coords:
-                return -2
-
-    except region.RegionHeaderError:
-        return -1
-		
-    except region.ChunkDataError:
-        return -1
-
-    except region.ChunkHeaderError:
-        return -1
-		
-    return chunk
-
-def check_level_file(level_file):
-    """ At the moment only tries to read a level.dat file and print a
-    warning if there are problems. """
-    try:
-        level_dat = nbt.NBTFile(filename = level_file)
-        print "\'level.dat\' is readable."
-        del level_dat
-        
-    except Exception, e:
-        print  "[WARNING!] The level.dat may be corrupted. Exception: \'{0}\'".format(e)
-
-def check_player_file(player_file):
-    """ At the moment only tries to read a .dat player file. it returns
-    0 if it's ok and 1 if has some problem """
-    try:
-        player_dat = nbt.NBTFile(filename = player_file)
-        del player_dat
-        return 0
-        
-    except Exception, e:
-        print  "[WARNING!] The player file {0} may be corrupted. Exception: \'{1}\'".format(player_file, e)
-        return 1
-    
 
 def get_global_chunk_coords(region_filename, chunkX, chunkZ):
     """ Takes the region filename and the chunk local 
@@ -367,9 +247,10 @@ def main():
 
 
     # do things with the option args
-    level_dat_filename = join(world_path, "level.dat")
-    player_files = glob(join(join(world_path, "players"), "*.dat"))
+    #~ level_dat_filename = join(world_path, "level.dat")
+    #~ player_files = glob(join(join(world_path, "players"), "*.dat"))
 
+    
     backups = options.backups
     use_backups = False
     if backups: # create a list of directories containing the backup of the region files
@@ -382,29 +263,26 @@ def main():
             use_backups = True
 
 
-    world_region_dir = join(world_path,"region")
-    if not exists(world_region_dir):
-        print "Error: Doesn't look like a minecraft world directory!"
-        sys.exit()
+    print "Scanning directory..."
 
-    world_nether_region_dir = join(world_path,"DIM-1/region")
-    if not exists(world_nether_region_dir):
+    w = world.World(world_path)
+
+    if not w.normal_mcr_files:
+        print "Warning: No region files found in the \"region\" directory!"
+
+    if not w.nether_mcr_files:
         print "Info: No nether dimension in the world directory."
 
-    print "Scanning directory..."
-    normal_region_files = glob(world_region_dir + "/r.*.*.mcr")
-    nether_region_files = glob(world_nether_region_dir + "/r.*.*.mcr")
-
-    if not normal_region_files and not nether_regioin_files:
-        print "Error: No region files found!"
+    if not w.normal_mcr_files and not w.nether_mcr_files:
+        print "Error: No region files to scan!"
         sys.exit()
-
-    if len(player_files) != 0:
-        print "There are {0} region files and {1} player files in the world directory.".format(len(normal_region_files) + len(nether_region_files), len(player_files))
+        
+    if w.player_files:
+        print "There are {0} region files and {1} player files in the world directory.".format(len(w.normal_mcr_files) + len(w.nether_mcr_files), len(w.player_files))
     else:
-        print "There are {0} region files in the world directory.".format(len(normal_region_files) + len(nether_region_files))
+        print "There are {0} region files in the world directory.".format(len(w.normal_mcr_files) + len(w.nether_mcr_files))
 
-    region_files = normal_region_files + nether_region_files
+    region_files = w.all_mcr_files
 
     # The program starts
     if options.delete_list: # Delete the given list of chunks
@@ -430,100 +308,33 @@ def main():
 
         print "\n{0:#^60}".format(' Scanning level.dat ')
 
-        if not exists(level_dat_filename):
+        if not w.level_file:
             print "[WARNING!] \'level.dat\' doesn't exist!"
         else:
-            check_level_file(level_dat_filename)
-        
+            scan_level(w)
+            if len(w.level_status) == 0:
+                print "\'level.dat'\ is redable"
+            else:
+                print "Warning: \'level.dat\' is corrupted, error: {0}".format(w.level_problems["file"])
+
+
         print "\n{0:#^60}".format(' Scanning player files ')
         
-        problems = 0
-        for player in player_files:
-            problems += check_player_file(player)
+        if not w.player_files:
+            print "Info: No player files to scan."
+        else:
+            scan_all_players(w)
         
-        if not problems:
-            print "All player files are readable."
-
+            if not w.player_problems:
+                print "All player files are readable."
+            else:
+                for player in w.player_problems:
+                    print "Warning: Player file \"{0}.dat\" has problems: {1}".format(player, w.player_problems[player])
 
         # check for corrupted chunks
-
         print "\n{0:#^60}".format(' Scanning region files ')
 
-        total_chunks = 0
-        corrupted_chunks = []
-        wrong_located_chunks = []
-        total_regions = len(region_files)
-        counter_region = 0
-        pbar = progressbar.ProgressBar(
-            widgets=['Scanning: ', FractionWidget(), ' ', progressbar.Percentage(), ' ', progressbar.Bar(left='[',right=']'), ' ', progressbar.ETA()],
-            maxval=total_regions)
-        if abs(options.processes) > 1:
-            #there is probably a better way to pass these values but this works for now
-            q = multiprocessing.Queue()
-            counter_region = multiprocessing.Value('d', 0)
-            def _mp_pool_init(prog_counter,del_ents,ent_limit,q):
-                _mp_check_region_file.q = q
-                global delete_entities
-                delete_entities = del_ents
-                global entity_limit
-                entity_limit = ent_limit
-                global counter
-                counter = prog_counter
-            pool = multiprocessing.Pool(processes=options.processes, initializer=_mp_pool_init,
-                initargs=(counter_region,options.delete_entities,options.entity_limit,q))
-            if not options.verbose:
-                pbar.start()
-            #the chunksize (arg #3) is pretty arbitrary, could probably be tweeked for better performance
-            result = pool.map_async(_mp_check_region_file, region_files, max(1,(total_regions//options.processes)//8))
-            
-            # printing status
-            counter = 0
-            while not result.ready() or (q.qsize() > 0 and options.verbose):
-                #this loop should probably use result.wait(1) but i didn't want to take the time to figure out what wait() returns if it hits the timeout
-                time.sleep(0.5)
-                if options.verbose:
-                    if q.qsize() > 0:
-                        filename,corrupted, wrong, total = q.get()
-                        counter += 1
-                        stats = "(corrupted: {0}, wrong located: {1}, chunks: {2})".format( len(corrupted), len(wrong), total)
-                        print "Scanned {0: <15} {1:.<60} {2}/{3}".format(filename, stats, counter, total_regions)
-                else:
-                    pbar.update(counter_region.value)
-            
-            if not options.verbose: pbar.finish()
-            
-            # making the results redable for region-fixer
-            for r in result.get():
-                if r is not None:
-                    corrupted_chunks.extend(r[1])
-                    wrong_located_chunks.extend(r[2])
-                    total_chunks += r[3]
-        else:
-            if options.verbose == False:
-                pbar.start()
-            for region_path in region_files:
-                counter_region += 1
-                if options.verbose:
-                    print "Scanning {0}   ...  {1}/{2}".format(region_path, counter_region, total_regions)
-                else:
-                    pbar.update(counter_region)
-                
-                if getsize(region_path) != 0: # some region files are 0 bytes size! And minecraft seems to handle them without problem.
-                    region_file = region.RegionFile(region_path)
-                else:
-                    continue
-                filename, bad_list, wrong_list, chunks = check_region_file(region_file, options.delete_entities, options.entity_limit)
-    
-                corrupted_chunks.extend(bad_list)
-                wrong_located_chunks.extend(wrong_list)
-    
-                total_chunks += chunks
-            
-            if not options.verbose:    
-                pbar.finish()
-        print "\nFound {0} corrupted and {1} wrong located chunks of a total of {2}\n".format(
-            len(corrupted_chunks), len(wrong_located_chunks),total_chunks)
-
+        scan_all_mcr_files(w, options)
 
         # Try to fix corrupted chunks with the backup copy
         if use_backups and (corrupted_chunks or wrong_located_chunks):
