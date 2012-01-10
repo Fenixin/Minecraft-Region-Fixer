7292c6f9cd3120b2b838891899ff7908dd3b8ec5
Alejandro Aguilera <fenixin@lavabit.com>
2012-01-10 13:37:31 +0100

    Those aren't data files.

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from distutils.core import setup
 import nbt
 import py2exe
 
-setup(console=['region-fixer.py'], data_files=['scan.py','world.py','progressbar.py','COPYING.txt','README.rst','CONTRIBUTORS.txt'])
+setup(console=['region-fixer.py'], data_files=['COPYING.txt','README.rst','CONTRIBUTORS.txt'])
