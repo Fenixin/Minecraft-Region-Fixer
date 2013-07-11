821c7d54b82437968f8a56d8832f1060f64a4df8
Alejandro Aguilera <fenixin@lavabit.com>
2013-07-11 18:41:46 +0200

    Update setup.py for windows exe.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import nbt
 import py2exe
 import sys
 
-if sys.args[1] == "py2exe":
-    setup(console=['region-fixer.py'], data_files=['COPYING.txt','README.rst','CONTRIBUTORS.txt'])
+if sys.argv[1] == "py2exe":
+    setup(console=['region-fixer.py'], data_files=['COPYING.txt','README.rst','CONTRIBUTORS.txt','DONORS.txt'])
 else:
     print "Use \'python setup.py py2exe\' to build a windows executable."
