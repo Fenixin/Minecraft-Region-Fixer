ab174710b813f00279eab2a93fea19a9ed66490c
Alejandro Aguilera <fenixin@gmail.com>
2014-07-01 11:20:25 +0200

    Ooops... add version files.

---
+++ gui/version.py
@@ -0,0 +1,8 @@
+'''
+Created on 24/06/2014
+
+@author: Alejandro
+'''
+
+version_string = "0.0.1"
+version_numbers = version_string.split(".")
---
+++ regionfixer_core/version.py
@@ -0,0 +1,8 @@
+'''
+Created on 24/06/2014
+
+@author: Alejandro
+'''
+
+version_string = "0.2.0"
+version_numbers = version_string.split('.')
